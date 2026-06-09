1f3c8c71235b1751a1647edbb3df44af7a0f6886
Luke <lukeontheearth@gmail.com>
2026-06-09 04:09:20 -0600

    Create CTASection.jsx

---
+++ src/components/home/CTASection.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { ArrowRight } from 'lucide-react';
+
+export default function CTASection() {
+  return null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
