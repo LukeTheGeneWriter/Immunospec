78f820afeeb5aeab6b71e2fb2fc5371b5747d371
Luke <lukeontheearth@gmail.com>
2026-06-11 06:35:34 -0600

    Update Vite config for base path and build settings
    Set base path and configure build output directory.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', //Impfix
+  build: {
+    outDir: 'dist', //impfix github pages
+    assestsDir: 'assets', //maybe needs to be componenets
+  },
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
